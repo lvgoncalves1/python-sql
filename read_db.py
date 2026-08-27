438f58e0c140d72bcb1b42c3fe2bda74b86f838f
lucas <lucasverissimo11799@gmail.com>
2026-08-27 15:24:23 -0300

    adiciona read_db

---
+++ read_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conn  = sqlite3.connect("escola.db")
+
+cursor = conn.cursor()
+
+cursor.execute(
+    """
+        SELECT * FROM estudantes
+    """
+)
+
+conn.commit()
+
+estudantes = cursor.fetchall()
+
+for estudante in estudantes:
+    print(estudante)
+
+conn.close()
