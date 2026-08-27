a6feec552b3a324774fae5e3dd9ee2f35dfe928e
lucas <lucasverissimo11799@gmail.com>
2026-08-27 15:20:13 -0300

    adciciona write_db

---
+++ write_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn  = sqlite3.connect("escola.db")
+
+cursor = conn.cursor()
+
+
+cursor.execute(
+    """
+        INSERT INTO estudantes(nome, idade) \
+        VALUES (?, ?)
+    """,
+    ("Joana", 30)
+)
+
+conn.commit()
+conn.close()
