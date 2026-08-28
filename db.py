6cdda378e0e4ed635b05468e4bee22d04fa98b32
lucas <lucasverissimo11799@gmail.com>
2026-08-28 10:30:55 -0300

    adiciona listar estudantes e criar estudantes

--- db.py
+++ db.py
@@ -18,3 +18,49 @@ def criar_tabela_estudante():
     )
     conn.commit()
     conn.close()
+
+def criar_tabela_matricula():
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+    """
+        CREATE TABLE IF NOT EXISTS matricula(
+            id INTEGER PRIMARY KEY,
+            nome_disciplina TEXT,
+            estudante_id INTEGER,
+            FOREIGN KEY (estudante_id) \
+                REFERENCES estudante(id)
+        )
+    """
+    )
+    conn.commit()
+    conn.close()
+
+def criar_estudante(nome, idade):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+    """
+       INSERT INTO estudantes (nome, idade) \ 
+       VALUES (?, ?)
+    """,
+        (nome, idade)
+    )
+    conn.commit()
+    conn.close()  
+
+def listar_estudantes():
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+    """
+       SELECT * FROM estudantes
+    """
+    )
+    estudantes = cursor.fetchall
+
+    for estudante in estudantes:
+        print(estudante)
+
+    conn.commit()
+    conn.close()  
