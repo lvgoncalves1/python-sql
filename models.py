c21029ac23de3414c5866549afa67a462bebea54
lucas <lucasverissimo11799@gmail.com>
2026-08-28 11:48:15 -0300

    cria classes estudante e matricula

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import \
+    Column, Integer, String, ForeignKey
+from db import Base
+
+class Estudante(Base):
+    __tablename__ = 'estudantes'
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(100), nullable=False)
+    idade = Column(Integer)
+
+class Matricula(Base):
+    __tablename__ = 'matriculas'
+    id = Column(Integer, primary_key=True, index=True)
+    student_id = Column(Integer, ForeignKey('estudantes.id'))
+    nome_disciplina = Column(String(100), nullable=False)
