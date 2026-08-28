6fcfc74825b0c0e8d5628d017d83df287d326729
lucas <lucasverissimo11799@gmail.com>
2026-08-28 11:41:22 -0300

    reformula db para postgres

--- db.py
+++ db.py
@@ -1,95 +1,10 @@
-import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import sessionmaker
 
-def conectar():
-    conn = sqlite3.connect("escola.db")
-    return conn
+DATABASE_URL =  "postgresql://postgres>:postgres@localhost/escola"
 
-def criar_tabela_estudante():
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute(
-    """
-        CREATE TABLE IF NOT EXISTS estudantes(
-            id INTEGER PRIMARY KEY,
-            nome TEXT,
-            idade INTEGER
-        )
-    """
-    )
-    conn.commit()
-    conn.close()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 
-def criar_tabela_matricula():
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute(
-    """
-        CREATE TABLE IF NOT EXISTS matriculas(
-            id INTEGER PRIMARY KEY,
-            estudante_id INTEGER,
-            nome_disciplina TEXT,
-            FOREIGN KEY (estudante_id) \
-                REFERENCES estudante(id)
-        )
-    """
-    )
-    conn.commit()
-    conn.close()
-
-def criar_estudante(nome, idade):
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute(
-    """
-       INSERT INTO estudantes (nome, idade) \
-       VALUES (?, ?)
-    """,
-        (nome, idade)
-    )
-    conn.commit()
-    conn.close()  
-
-def listar_estudantes():
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute(
-    """
-       SELECT * FROM estudantes
-    """
-    )
-    estudantes = cursor.fetchall
-
-    for estudante in estudantes:
-        print(estudante)
-
-    conn.commit()
-    conn.close()  
-
-def criar_matricula(estudante_id, nome_disciplina):
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute(
-    """
-       INSERT INTO matriculas (estudante_id, nome_disciplina) \
-       VALUES (?, ?)
-    """,
-        (estudante_id, nome_disciplina)
-    )
-
-def listar_matriculas():
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute(
-    """
-       SELECT matriculas.id, estudantes.nome, matriculas.nome_disciplina
-       FROM matriculas
-       JOIN estudantes ON matriculas.estudante_id = estudantes.id
-    """
-    )
-    matriculas = cursor.fetchall
-
-    for matricula in matriculas:
-        print(matricula)
-
-    conn.commit()
-
+Base = declarative_base()
