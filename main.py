9f702daf4676fd145ae8c8255293ecc06080872b
lucas <lucasverissimo1799@gmail.com>
2026-08-28 16:05:44 -0300

    adiciona schemas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 import models, schemas
 from database import engine, SessionLocal
